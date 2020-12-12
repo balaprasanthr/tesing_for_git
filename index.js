52871562cc6797f87305acec43dbed29a8ad291c
balaprasanthr <balaprasanthr@gmail.com>
2020-12-12 13:01:15 +0530

    Tesing of adding files

---
+++ index.js
@@ -0,0 +1,70 @@
+// OOP - Object Oriented Programming
+// Style of Programming or a Programming paradigm.
+// Encapsulation 
+// Abstraction 
+// Inheritence 
+// Polymorphism
+// 
+
+
+// console.log('Hello');
+
+// ==================================================
+// Objects
+// Creating Objects
+// Factories and Constructors
+// Primitives and Reference Types
+// Working With Properties
+// Private Properties
+// Getters/Setters
+
+//==================================================
+// Object Literals
+
+// const circle = {
+// 	radius : 1,
+// 	location: {
+// 		x: 1,
+// 		y: 1
+// 	},
+// 	draw: function(){
+// 		console.log('Draw');
+// 	}
+// };
+
+// circle.draw();
+// =================================================
+
+// Factories
+const circle = {
+	radius : 1,
+	location: {
+		x: 1,
+		y: 1
+	},
+	draw: function(){
+		console.log('Draw');
+	}
+};
+
+circle.draw();
+// =================================================
+
+//Constructors
+
+// =================================================
+
+// Constructor Property
+
+// =================================================
+
+// Functions are Objects
+
+// =================================================
+// =================================================
+// =================================================
+// =================================================
+// =================================================
+// =================================================
+// =================================================
+// =================================================
